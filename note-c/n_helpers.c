6b2679e72300c756e8f41317227fb1d12ca2792d
Brandon Satrom <bsatrom+gh@gmail.com>
2020-12-28 15:11:24 -0600

    update note-c

--- note-c/n_helpers.c
+++ note-c/n_helpers.c
@@ -789,9 +789,10 @@ bool NoteGetStatusST(char *statusBuf, int statusBufLen, JTIME *bootTime, bool *r
 bool NoteSleep(char *stateb64, uint32_t seconds, const char *modes) {
     bool success = false;
 
-    // Put ourselves to sleep
+    // Use a Command rather than a Request so that the Notecard doesn't try to send
+    // a response back to us, which would cause a communications error on that end.
     _Debug("requesting sleep\n");
-    J *req = NoteNewRequest("card.attn");
+    J *req = NoteNewCommand("card.attn");
     if (req != NULL) {
         // Add the base64 item in a wonderful way that doesn't strdup the huge string
         J *stringReferenceItem = JCreateStringReference(stateb64);
@@ -1215,8 +1216,8 @@ bool NoteGetContact(char *nameBuf, int nameBufLen, char *orgBuf, int orgBufLen, 
                 strlcpy(roleBuf, JGetString(rsp, "role"), roleBufLen);
             if (emailBuf != NULL)
                 strlcpy(emailBuf, JGetString(rsp, "email"), emailBufLen);
-            NoteDeleteResponse(rsp);
         }
+        NoteDeleteResponse(rsp);
     }
 
     return success;
